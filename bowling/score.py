2633cd9eb341bd0dee238dc725ae18bdee6aa46c
Abdul Gani <abdulg@gmail.com>
2019-11-24 17:43:57 -0500

    Refactor spare and strike handling into methods.

--- bowling/score.py
+++ bowling/score.py
@@ -51,34 +51,40 @@ class Frame():
             return 0
 
         elif score_string[0] == '/':
-            partial = 10 - self.pins1
-            if score_string[1].isdigit():
-                return partial + int(score_string[1])
-
-            elif score_string[1] == '-':
-                return partial
-
-            elif score_string[1] == 'X':
-                return partial + 10
+            return self.handle_spare(score_string)
 
         elif score_string[0] == 'X':
-            partial = 10
-            first_bonus_ball = 0
-            if score_string[1].isdigit():
-                first_bonus_ball = int(score_string[1])
-                partial += first_bonus_ball
-            elif score_string[1] == '-':
-                first_bonus_ball = 0
-            elif score_string[1] == 'X':
-                partial += 10
+            return self.handle_strike(score_string)
 
-            if score_string[2].isdigit():
-                partial += int(score_string[2])
-            elif score_string[2] == '-':
-                pass
-            elif score_string[2] == '/':
-                partial += 10 - first_bonus_ball
-            elif score_string[2] == 'X':
-                partial += 10
+    def handle_spare(self, score_string):
+        partial = 10 - self.pins1
+        if score_string[1].isdigit():
+            return partial + int(score_string[1])
 
+        elif score_string[1] == '-':
             return partial
+
+        elif score_string[1] == 'X':
+            return partial + 10
+
+    def handle_strike(self, score_string):
+        partial = 10
+        first_bonus_ball = 0
+        if score_string[1].isdigit():
+            first_bonus_ball = int(score_string[1])
+            partial += first_bonus_ball
+        elif score_string[1] == '-':
+            first_bonus_ball = 0
+        elif score_string[1] == 'X':
+            partial += 10
+
+        if score_string[2].isdigit():
+            partial += int(score_string[2])
+        elif score_string[2] == '-':
+            pass
+        elif score_string[2] == '/':
+            partial += 10 - first_bonus_ball
+        elif score_string[2] == 'X':
+            partial += 10
+
+        return partial
