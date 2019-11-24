59dbb9b839708faa84d6373a84d6b63c87d1502a
Abdul Gani <abdulg@gmail.com>
2019-11-24 09:52:48 -0500

    Parametrize the test

--- tests/test_bowling.py
+++ tests/test_bowling.py
@@ -1,6 +1,16 @@
-import pytest
 from bowling.score import calculate_score
+import pytest
+
+@pytest.fixture(params= [
+    {
+        'score': '1111111111',
+        'expected': 10
+    },
+])
+def scores_to_test(request):
+    yield request.param
+
 
 class TestBowling:
-    def test_calculate_score(self):
-        assert calculate_score('1111111111') == 10
+    def test_calculate_score(self, scores_to_test):
+        assert calculate_score(scores_to_test['score']) == scores_to_test['expected']
